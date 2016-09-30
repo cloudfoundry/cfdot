569c1e29dd2b5ab3a95277308fd13e94905818c1
Brandon Shroyer <bshroyer@pivotal.io>
2016-09-30 17:34:25 -0400

    Wrap desired_lrp tests in a Context
    * JustBeforeEach in desired_lrp_test was accidentally being run by the BBS
    flag validation test, causing timeouts.
    
    Signed-off-by: Joachim Valdez <valdezj@us.ibm.com>

--- integration/desired_lrp_test.go
+++ integration/desired_lrp_test.go
@@ -21,123 +21,124 @@ var _ = Describe("desired-lrp", func() {
 
 	itValidatesBBSFlags("desired-lrp", "test-guid")
 
-	JustBeforeEach(func() {
-		args = append([]string{"--bbsURL", bbsServer.URL(), "desired-lrp"}, args...)
-		cfdotCmd := exec.Command(cfdotPath, args...)
+	Context("when BBS flags are valid", func() {
+		JustBeforeEach(func() {
+			args = append([]string{"--bbsURL", bbsServer.URL(), "desired-lrp"}, args...)
+			cfdotCmd := exec.Command(cfdotPath, args...)
 
-		var err error
-		sess, err = gexec.Start(cfdotCmd, GinkgoWriter, GinkgoWriter)
-		Expect(err).NotTo(HaveOccurred())
+			var err error
+			sess, err = gexec.Start(cfdotCmd, GinkgoWriter, GinkgoWriter)
+			Expect(err).NotTo(HaveOccurred())
 
-		Eventually(sess.Exited).Should(BeClosed())
-	})
-
-	Context("when no arguments are provided", func() {
-		BeforeEach(func() {
-			args = []string{}
+			Eventually(sess.Exited).Should(BeClosed())
 		})
 
-		It("fails with exit code 3", func() {
-			Expect(sess.ExitCode()).To(Equal(3))
-		})
-		It("prints usage to stderr", func() {
-			Expect(sess.Err).To(gbytes.Say("Missing arguments"))
-			Expect(sess.Err).To(gbytes.Say("cfdot desired-lrp PROCESS_GUID \\[flags\\]"))
-		})
-	})
-
-	Context("when two arguments are provided", func() {
-		BeforeEach(func() {
-			args = []string{"arg1", "arg2"}
-		})
-
-		It("fails with exit code 3", func() {
-			Expect(sess.ExitCode()).To(Equal(3))
-		})
-		It("prints usage to stderr", func() {
-			Expect(sess.Err).To(gbytes.Say("Too many arguments specified"))
-			Expect(sess.Err).To(gbytes.Say("cfdot desired-lrp PROCESS_GUID \\[flags\\]"))
-		})
-	})
-
-	Context("when an empty argument is provided", func() {
-		BeforeEach(func() {
-			args = []string{""}
-		})
-
-		It("fails with exit code 3", func() {
-			Expect(sess.ExitCode()).To(Equal(3))
-		})
-		It("prints usage to stderr", func() {
-			Expect(sess.Err).To(gbytes.Say("Process guid should be non empty string"))
-			Expect(sess.Err).To(gbytes.Say("cfdot desired-lrp PROCESS_GUID \\[flags\\]"))
-		})
-	})
-
-	Context("when a desired-lrp process_guid is provided", func() {
-		var (
-			desiredLRP *models.DesiredLRP
-		)
-		BeforeEach(func() {
-			args = []string{"test-guid"}
-		})
-		Context("when bbs responds with 200 status code", func() {
+		Context("when no arguments are provided", func() {
 			BeforeEach(func() {
-				desiredLRP = &models.DesiredLRP{
-					ProcessGuid: "test-guid",
-					Instances:   2,
-				}
-
-				bbsServer.AppendHandlers(
-					ghttp.CombineHandlers(
-						ghttp.VerifyRequest("POST", "/v1/desired_lrps/get_by_process_guid.r2"),
-						ghttp.VerifyProtoRepresenting(&models.DesiredLRPByProcessGuidRequest{
-							ProcessGuid: "test-guid",
-						}),
-						ghttp.RespondWithProto(200, &models.DesiredLRPResponse{
-							DesiredLrp: desiredLRP,
-							Error:      nil,
-						}),
-					),
-				)
-
+				args = []string{}
 			})
 
-			It("exits with status code of 0", func() {
-				Expect(sess.ExitCode()).To(Equal(0))
+			It("fails with exit code 3", func() {
+				Expect(sess.ExitCode()).To(Equal(3))
 			})
-
-			It("returns the json encoding of the desired lrp scheduling info", func() {
-				jsonData, err := json.Marshal(desiredLRP)
-				Expect(err).NotTo(HaveOccurred())
-
-				Expect(sess.Out).To(gbytes.Say(string(jsonData)))
+			It("prints usage to stderr", func() {
+				Expect(sess.Err).To(gbytes.Say("Missing arguments"))
+				Expect(sess.Err).To(gbytes.Say("cfdot desired-lrp PROCESS_GUID \\[flags\\]"))
 			})
 		})
 
-		Context("when bbs responds with non-200 status code", func() {
+		Context("when two arguments are provided", func() {
 			BeforeEach(func() {
-				bbsServer.AppendHandlers(
-					ghttp.CombineHandlers(
-						ghttp.VerifyRequest("POST", "/v1/desired_lrps/get_by_process_guid.r2"),
-						ghttp.RespondWithProto(500, &models.DesiredLRPResponse{
-							Error: &models.Error{
-								Type:    models.Error_Deadlock,
-								Message: "deadlock detected",
-							},
-						}),
-					),
-				)
+				args = []string{"arg1", "arg2"}
 			})
 
-			It("exits with status code of 4", func() {
-				Expect(sess.ExitCode()).To(Equal(4))
+			It("fails with exit code 3", func() {
+				Expect(sess.ExitCode()).To(Equal(3))
 			})
-
-			It("prints the error", func() {
-				Expect(sess.Err).To(gbytes.Say("deadlock"))
+			It("prints usage to stderr", func() {
+				Expect(sess.Err).To(gbytes.Say("Too many arguments specified"))
+				Expect(sess.Err).To(gbytes.Say("cfdot desired-lrp PROCESS_GUID \\[flags\\]"))
 			})
 		})
 
+		Context("when an empty argument is provided", func() {
+			BeforeEach(func() {
+				args = []string{""}
+			})
+
+			It("fails with exit code 3", func() {
+				Expect(sess.ExitCode()).To(Equal(3))
+			})
+			It("prints usage to stderr", func() {
+				Expect(sess.Err).To(gbytes.Say("Process guid should be non empty string"))
+				Expect(sess.Err).To(gbytes.Say("cfdot desired-lrp PROCESS_GUID \\[flags\\]"))
+			})
+		})
+
+		Context("when a desired-lrp process_guid is provided", func() {
+			var (
+				desiredLRP *models.DesiredLRP
+			)
+			BeforeEach(func() {
+				args = []string{"test-guid"}
+			})
+			Context("when bbs responds with 200 status code", func() {
+				BeforeEach(func() {
+					desiredLRP = &models.DesiredLRP{
+						ProcessGuid: "test-guid",
+						Instances:   2,
+					}
+
+					bbsServer.AppendHandlers(
+						ghttp.CombineHandlers(
+							ghttp.VerifyRequest("POST", "/v1/desired_lrps/get_by_process_guid.r2"),
+							ghttp.VerifyProtoRepresenting(&models.DesiredLRPByProcessGuidRequest{
+								ProcessGuid: "test-guid",
+							}),
+							ghttp.RespondWithProto(200, &models.DesiredLRPResponse{
+								DesiredLrp: desiredLRP,
+								Error:      nil,
+							}),
+						),
+					)
+
+				})
+
+				It("exits with status code of 0", func() {
+					Expect(sess.ExitCode()).To(Equal(0))
+				})
+
+				It("returns the json encoding of the desired lrp scheduling info", func() {
+					jsonData, err := json.Marshal(desiredLRP)
+					Expect(err).NotTo(HaveOccurred())
+
+					Expect(sess.Out).To(gbytes.Say(string(jsonData)))
+				})
+			})
+
+			Context("when bbs responds with non-200 status code", func() {
+				BeforeEach(func() {
+					bbsServer.AppendHandlers(
+						ghttp.CombineHandlers(
+							ghttp.VerifyRequest("POST", "/v1/desired_lrps/get_by_process_guid.r2"),
+							ghttp.RespondWithProto(500, &models.DesiredLRPResponse{
+								Error: &models.Error{
+									Type:    models.Error_Deadlock,
+									Message: "deadlock detected",
+								},
+							}),
+						),
+					)
+				})
+
+				It("exits with status code of 4", func() {
+					Expect(sess.ExitCode()).To(Equal(4))
+				})
+
+				It("prints the error", func() {
+					Expect(sess.Err).To(gbytes.Say("deadlock"))
+				})
+			})
+		})
 	})
 })
