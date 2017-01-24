b7834a23a1b7590d5e7a0522699f08f9a9820fff
Nima Kaviani <nkavian@us.ibm.com>
2017-01-24 14:43:24 -0800

    add new command create-task
    [#129376223]
    
    Signed-off-by: Danny Cao <dcao@us.ibm.com>

---
+++ commands/create_task.go
@@ -0,0 +1,93 @@
+package commands
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"code.cloudfoundry.org/bbs"
+	"code.cloudfoundry.org/bbs/models"
+	"github.com/spf13/cobra"
+)
+
+var createTaskCmd = &cobra.Command{
+	Use:   "create-task (spec|@file)",
+	Short: "Create a Task",
+	Long:  "Create a Task from the given spec. Spec can either be json encoded task, e.g. '{\"task_guid\":\"some-guid\"}' or a file containing json encoded task, e.g. @/path/to/spec/file",
+	RunE:  createTask,
+}
+
+func init() {
+	AddBBSFlags(createTaskCmd)
+	RootCmd.AddCommand(createTaskCmd)
+}
+
+func createTask(cmd *cobra.Command, args []string) error {
+	if len(args) != 1 {
+		return NewCFDotValidationError(cmd, fmt.Errorf("missing spec argument"))
+	}
+
+	spec, err := ValidateCreateTaskArguments(args)
+	if err != nil {
+		return NewCFDotValidationError(cmd, err)
+	}
+
+	bbsClient, err := newBBSClient(cmd)
+	if err != nil {
+		return NewCFDotError(cmd, err)
+	}
+
+	err = CreateTask(cmd.OutOrStdout(), cmd.OutOrStderr(), bbsClient, spec)
+	if err != nil {
+		return NewCFDotError(cmd, err)
+	}
+
+	return nil
+}
+
+func ValidateCreateTaskArguments(args []string) ([]byte, error) {
+	var spec []byte
+	var err error
+	var task *models.Task
+
+	argValue := args[0]
+	if strings.HasPrefix(argValue, "@") {
+		_, err := os.Stat(argValue[1:])
+		if err != nil {
+			return nil, err
+		}
+		spec, err = ioutil.ReadFile(argValue[1:])
+		if err != nil {
+			return nil, err
+		}
+
+	} else {
+		spec = []byte(argValue)
+	}
+	err = json.Unmarshal([]byte(spec), &task)
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("Invalid JSON: %s", err.Error()))
+	}
+	return spec, nil
+}
+
+func CreateTask(stdout, stderr io.Writer, bbsClient bbs.Client, spec []byte) error {
+	logger := globalLogger.Session("create-task")
+
+	var task *models.Task
+	err := json.Unmarshal(spec, &task)
+	if err != nil {
+		return err
+	}
+
+	err = bbsClient.DesireTask(logger, task.TaskGuid, task.Domain, task.TaskDefinition)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ integration/create_task_test.go
@@ -0,0 +1,168 @@
+package integration_test
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"os/exec"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"code.cloudfoundry.org/bbs/models"
+	"github.com/onsi/gomega/gbytes"
+	"github.com/onsi/gomega/gexec"
+	"github.com/onsi/gomega/ghttp"
+)
+
+var _ = Describe("create-task", func() {
+	var sess *gexec.Session
+
+	itValidatesBBSFlags("create-task")
+
+	Context("when no spec is passed", func() {
+		JustBeforeEach(func() {
+			cfdotCmd := exec.Command(cfdotPath, "--bbsURL", bbsServer.URL(), "create-task")
+
+			var err error
+			sess, err = gexec.Start(cfdotCmd, GinkgoWriter, GinkgoWriter)
+			Expect(err).NotTo(HaveOccurred())
+
+			Eventually(sess.Exited).Should(BeClosed())
+		})
+
+		It("exits with status code of 3 and prints the error and usage", func() {
+			Eventually(sess).Should(gexec.Exit(3))
+			Expect(sess.Err).To(gbytes.Say(`missing spec`))
+			Expect(sess.Err).To(gbytes.Say("cfdot create-task \\(spec\\|@file\\) .*"))
+		})
+	})
+
+	Context("when bbs responds with 200 status code", func() {
+		var (
+			task *models.Task
+			args []string
+		)
+
+		BeforeEach(func() {
+			task = &models.Task{
+				TaskGuid: "some-task-guid",
+			}
+		})
+
+		JustBeforeEach(func() {
+			bbsServer.AppendHandlers(
+				ghttp.CombineHandlers(
+					ghttp.VerifyRequest("POST", "/v1/tasks/desire.r2"),
+					ghttp.VerifyProtoRepresenting(&models.DesireTaskRequest{
+						TaskGuid:       task.TaskGuid,
+						Domain:         task.Domain,
+						TaskDefinition: task.TaskDefinition,
+					}),
+					ghttp.RespondWithProto(200, &models.TaskLifecycleResponse{
+						Error: nil,
+					}),
+				),
+			)
+
+			args = append([]string{"--bbsURL", bbsServer.URL(), "create-task"}, args...)
+			cfdotCmd := exec.Command(
+				cfdotPath,
+				args...,
+			)
+			var err error
+			sess, err = gexec.Start(cfdotCmd, GinkgoWriter, GinkgoWriter)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		Context("as json", func() {
+			BeforeEach(func() {
+				spec, err := json.Marshal(task)
+				Expect(err).NotTo(HaveOccurred())
+				args = []string{string(spec)}
+			})
+
+			It("exits with status code of 0", func() {
+				Eventually(sess).Should(gexec.Exit(0))
+			})
+		})
+
+		Context("as a file", func() {
+			BeforeEach(func() {
+				spec, err := json.Marshal(task)
+				Expect(err).NotTo(HaveOccurred())
+				f, err := ioutil.TempFile(os.TempDir(), "desired_task_spec")
+				Expect(err).NotTo(HaveOccurred())
+				defer f.Close()
+				_, err = f.Write(spec)
+				Expect(err).NotTo(HaveOccurred())
+				args = []string{"@" + f.Name()}
+			})
+
+			It("exits with status code 0", func() {
+				Eventually(sess).Should(gexec.Exit(0))
+			})
+		})
+
+		Context("empty spec", func() {
+			BeforeEach(func() {
+				args = nil
+			})
+
+			It("exits with status code of 3", func() {
+				Eventually(sess).Should(gexec.Exit(3))
+			})
+		})
+
+		Context("invalid spec", func() {
+			BeforeEach(func() {
+				args = []string{"foo"}
+			})
+
+			It("exits with status code of 3 and prints the error", func() {
+				Eventually(sess).Should(gexec.Exit(3))
+				Expect(sess.Err).To(gbytes.Say("Invalid JSON:"))
+			})
+		})
+
+		Context("non-existing spec file", func() {
+			BeforeEach(func() {
+				args = []string{"@/path/to/non/existing/file"}
+			})
+
+			It("exits with status 3 and prints the error", func() {
+				Eventually(sess).Should(gexec.Exit(3))
+				Expect(sess.Err).To(gbytes.Say("no such file"))
+			})
+		})
+	})
+
+	Context("when bbs responds with non-200 status code", func() {
+		JustBeforeEach(func() {
+			bbsServer.AppendHandlers(
+				ghttp.CombineHandlers(
+					ghttp.VerifyRequest("POST", "/v1/tasks/desire.r2"),
+					ghttp.RespondWithProto(500, &models.TaskLifecycleResponse{
+						Error: &models.Error{
+							Type:    models.Error_Deadlock,
+							Message: "deadlock detected",
+						},
+					}),
+				),
+			)
+
+			cfdotCmd := exec.Command(
+				cfdotPath,
+				"--bbsURL", bbsServer.URL(), "create-task", "{}",
+			)
+			var err error
+			sess, err = gexec.Start(cfdotCmd, GinkgoWriter, GinkgoWriter)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("exits with status code 4 and prints the error", func() {
+			Eventually(sess).Should(gexec.Exit(4))
+			Expect(sess.Err).To(gbytes.Say("deadlock"))
+		})
+	})
+})
